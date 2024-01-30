8180a39efc92a61479648b9d57b8387cf9a57f64
Kennedy-oss <kennedy.onyango4@gmail.com>
2024-01-30 19:10:51 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+# In lib/dog.py
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Fido")  # Breed will be "Mutt"
+buddy = Dog("Buddy", "Golden Retriever")  # Breed will be "Golden Retriever"
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+# In lib/person.py
+
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+alice = Person("Alice")  # This creates a Person object with the name "Alice"
+
